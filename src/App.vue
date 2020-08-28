e82caee4b35a8c0ccc9b7d526de44d5a062ff8c8
ukonw303 <gameger005@gmail.com>
2020-08-28 12:29:02 +0700

    di

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+    <HelloWorld msg="U LIKE THIS ?" />
+    <br>
+    <div class="row">
+      <div class="col-12">
+      <img src="https://freedom.ilaw.or.th/sites/default/files/imagecache/wysiwyg_imageupload_colorbox_preset/wysiwyg_imageupload/1/02_3.jpg" width="200" height="200" />
+        <h1>{{total1}}</h1>
+        <div class="col-12">
+          <CsButton @cc="sum" /><br>
+        </div>
+      </div>
+      <div class="col-12">
+      <img src="https://media0.giphy.com/media/3osxYuTGietZW1qBfa/200w.webp?cid=ecf05e47cykyeupgh4706qlu3wqqi2tb9vocpp33lgdd8h03&rid=200w.webp" width="200" height="200" />
+        <h1>{{total2}}</h1>
+        <div class="col-12">
+          <CsButton @cc="sum1" />
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CsButton";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sum1(counterValue){
+    this.total2 = counterValue
+},
+},
+
+};
+
 </script>
 
 <style>
+body {
+      background-image: url(https://media1.tenor.com/images/8e46150f790fbefe438d9c2767c32ad1/tenor.gif?itemid=15673753);
+      background-size: 100px 100px ;
+    }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Londrina Solid', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+  margin-top: 70px;
 }
 </style>
---
+++ src/components/CsButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-primary">LIKE {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-info ml-3">LOVE {{lable}}</button>
+    <button v-on:click="downFunction" class="btn  btn-danger ml-3">DIS LINE {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-danger ml-3">RESET {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+     LoveFunction: function () {
+      this.counter += 15;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
